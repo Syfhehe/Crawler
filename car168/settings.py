e43c306a7430d5ca14aa647e46c4ea115a9bc8be
jinyu <yu.jin@ximalaya.com>
2019-04-14 15:06:46 +0800

    add first car168

---
+++ car168/settings.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : settings.py
+# @Author: yu.jin
+# @Date  : 2019-04-14
+# @Desc  :
+
+
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36'
+
