0551c076c2bb3a56b873464380131bde836e737c
Yi Fan Shen <syfshen@cn.ibm.com>
2019-05-01 19:32:34 +0800

    Update selenium168_new.py

--- car168/selenium168_new.py
+++ car168/selenium168_new.py
@@ -35,7 +35,7 @@ name = driver.find_element_by_name("uname")
 # 刘树
 # name.send_keys("17721338625")
 # 唐师兄
-name.send_keys("18731097547")
+name.send_keys("15731044734")
 
 
 script = "Object.defineProperties(navigator,{webdriver:{get:() => false}});"
@@ -52,7 +52,7 @@ ActionChains(driver).move_by_offset(xoffset=250, yoffset=0).perform()
 time.sleep(0.5)
 # 释放滑块
 ActionChains(driver).release().perform()
-time.sleep(2)
+time.sleep(3)
 driver.find_element_by_id("sendCode").click()
 
 
@@ -66,147 +66,11 @@ while driver.current_url != "http://www.chehang168.com/index.php?c=index&m=index
 print(driver.current_url)
 time.sleep(1)
 
-# 品牌爬取
-# driver.get("http://www.chehang168.com/index.php?c=index&m=allBrands")
-# brand_urls = []
-# for i in range(25):
-#     # u"/html/body/div[4]/div/ul/li[*]/a[*]"
-#     brand_xpath = "/html/body/div[4]/div/ul/li[*]/a[%d]"%i
-#     brands = driver.find_elements_by_xpath(brand_xpath)
-#     for url in brands:
-#         brand_urls.append((url.text, url.get_attribute("href"), "TODO"))
-#     # brand_file_path = "/Users/yu.jin/Downloads/chehang168_brand_0418.txt"
-#     # with open(brand_file_path, "w") as f:  # 设置文件对象
-#     #     brands_name_url = ["%s,%s" % (line[0], line[1]) + "\n" for line in brand_urls]
-#     #     f.writelines(brands_name_url)
-#
-# insert_data_brand(brand_urls)
-
-# TODO, 品牌链接与品牌名称 数据库保存
-
-
-# 系列爬取
-# series_urls = []
-# for brand_url in brand_urls:
-#     driver.get(brand_url[1])
-#     series_xpath = "/html/body/div[4]/div[1]/div[1]/div/div[2]/div/div[*]/ul/li[*]/a"
-#     series = driver.find_elements_by_xpath(series_xpath)
-#     for url in series:
-#         series_urls.append((url.text, url.get_attribute("href") + "&type=1", "TODO"))  # type=1 代表选中公司
-#
-#     # series_file_path = "/Users/yu.jin/Downloads/chehang168_series_0418.txt"
-#     # with open(series_file_path, "w") as f:  # 设置文件对象
-#     #     series_name_url = ["%s,%s" % (line[0], line[1]) + "\n" for line in series_urls]
-#     #     f.writelines(series_name_url)
-# insert_data_series(series_urls)
-
-# TODO, 系列链接与系列名称 数据库保存
-#
-
-# http://www.chehang168.com/index.php?c=index&m=series&psid=6aaIm&type=1&pricetype=0&page=2
-
-import numpy as np
-
-# series_urls_not_crawl = query_series_url_by_status("TODO")
-# series_urls_not_crawl = [url[0] for url in series_urls_not_crawl]
-# series_urls_crawl = set()
-
-# series_file_path = "/Users/yu.jin/Downloads/chehang168_series_0418.txt"
-# #
-# series_urls = []
-# with open(series_file_path, "r") as f:    #设置文件对象
-#     urls = f.readlines()
-#     for url in urls:
-#         series_urls.append(url[:-1].split(",")[1])
-#
-# # 经销商链接爬取
-
-# flag = 0
-# company_urls = set()
-# for series_url in series_urls_not_crawl:
-#     # TODO, 页码爬取
-#     driver.get(series_url + '&pricetype=0&page=1')
-#     # company_xpath = "/html/body/div[4]/div[1]/div[2]/ul[2]/li[*]/p[3]/a"
-#     # company = driver.find_elements_by_xpath(company_xpath)
-#
-#     last_page_number = 1
-#     try:
-#         link = driver.find_element_by_link_text(">>")
-#
-#         if link:
-#             link = link.get_attribute("href")
-#             last_page_number = int(link[link.find("page=")+5:])
-#     except Exception as e:
-#         driver.get(series_url + '&pricetype=0&page=1')  # 链接加上页码
-#         if driver.current_url == 'http://www.chehang168.com/index.php?c=com&m=limitPage':
-#             flag = 1
-#             break
-#         company_xpath = "/html/body/div[4]/div[1]/div[2]/ul[2]/li[*]/p[3]/a"
-#         company_xpath2 = "/html/body/div[4]/div[1]/div[2]/ul[2]/li[*]/p[2]/a"
-#         company = driver.find_elements_by_xpath(company_xpath)
-#         for url in company:
-#             # print("%s, %s" % (url.text, url.get_attribute("href")))
-#             company_urls.add((url.text, url.get_attribute("href"), "TODO"))
-#
-#         company2 = driver.find_elements_by_xpath(company_xpath2)
-#         for url in company2:
-#             # print("%s, %s" % (url.text, url.get_attribute("href")))
-#             company_urls.add((url.text, url.get_attribute("href"), "TODO"))
-#
-#         series_urls_crawl.add(series_url)
-#
-#     for page in range(last_page_number):
-#         driver.get(series_url + '&pricetype=0&page=' + "%s" % str(page+1))  # 链接加上页码
-#         if driver.current_url == 'http://www.chehang168.com/index.php?c=com&m=limitPage':
-#             flag = 1
-#             break
-# #
-#         company_xpath = "/html/body/div[4]/div[1]/div[2]/ul[2]/li[*]/p[3]/a"
-#         company_xpath2 = "/html/body/div[4]/div[1]/div[2]/ul[2]/li[*]/p[2]/a"
-#         company = driver.find_elements_by_xpath(company_xpath)
-#         for url in company:
-#             # print("%s, %s" % (url.text, url.get_attribute("href")))
-#             company_urls.add((url.text, url.get_attribute("href"), "TODO"))
-#
-#         company2 = driver.find_elements_by_xpath(company_xpath2)
-#         for url in company2:
-#             # print("%s, %s" % (url.text, url.get_attribute("href")))
-#             company_urls.add((url.text, url.get_attribute("href"), "TODO"))
-#
-#     if flag:
-#         series_urls_crawl.add(series_url)
-#         break
-#     else:
-#         series_urls_crawl.add(series_url)
-#
-# for company in company_urls:
-#     print("%s, %s, %s" % (company[0], company[1], company[2]))
-#
-# insert_data_seller(company_urls)
-#
-#
-# for url_crawl in series_urls_crawl:
-#     update_series_status_by_url(url_crawl, "DONE")
-
-# company_file_path = "/Users/yu.jin/Downloads/chehang168_company_0418.csv"
-# with open(company_file_path, "w") as f:  # 设置文件对象
-#     company_name_url = ["%s,%s" % (line[0], line[1]) + "\n" for line in company_urls]
-#     f.writelines(company_name_url)
-#
-# # TODO, 经销商名称爬取
-# # TODO, 经销商链接与厂商名称 保存
-#
-# # TODO, 经销商信息爬取
-
 company_urls = query_company_url_by_status("TODO")
 company_urls_not_crawl = [url[0] for url in company_urls]
-
-# company_urls_not_crawl = ["http://www.chehang168.com/u/ewxpt_027ppBdpZ"]
-
-flag = 0
-
 datas = []
 try:
+    time.sleep(2)
     for company_url in company_urls_not_crawl:
         driver.get(company_url)
         # 查看联系人 按钮点击
